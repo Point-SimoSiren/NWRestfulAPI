77edacf7d82325089eee92ca6a1abc9e4fa2e380
Simppa <simo.siren@careeria.fi>
2025-11-04 09:45:54 +0200

    luokkia kirjautumiseen

---
+++ NWRestfulAPI/Models/Credentials.cs
@@ -0,0 +1,8 @@
+﻿namespace NWRestfulAPI.Models
+{
+    public class Credentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ NWRestfulAPI/Models/LoggedUser.cs
@@ -0,0 +1,10 @@
+﻿namespace NWRestfulAPI.Models
+{
+    public class LoggedUser
+    {
+        public string Username { get; set; }
+        public int Accesslevel { get; set; }
+        public string? Token { get; set; }
+
+    }
+}
